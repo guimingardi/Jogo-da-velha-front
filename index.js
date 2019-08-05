85c06780d0cecee9180fa1c5deb9c44c816eabf4
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-08-05 15:05:09 -0300

    Modifica organizacao das pastas, adiciona gitignore

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
