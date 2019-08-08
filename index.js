8b69a78612252c7ea78613785e12c8425bb390a1
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-08-08 10:33:28 -0300

    Adiciona botao de iniciar e verifica qual e o jogador um e dois

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ const server = require('http').Server(express)
 const io = require('socket.io')(server)
 const port = 3000
 
+
+// to com dificuldade
+
 let jogoStatus = {
-  jogador1: "",
-  jogador2: "",
-  jogadas: 0
+   jogador1: "",
+   jogador2: "",
+   jogadas: 0
 }
 
 io.on('connection', function(socket){
   console.log(`Tentativa de conexão`)
-  socket.on('jogo_acao', function(msg){
+  socket.on('jogo_acao', function(msg) {
+    
+    if(msg.tipo === 'nome' && jogoStatus.jogador1 === '') {
+      jogoStatus.jogador1 = msg.dado
+
+      io.emit('socket_msg', {
+        tipo: 'jogador1',
+        dado: msg.dado
+      });
+
+      console.log('jogador 1', jogoStatus.jogador1)
+    } else if(msg.tipo === 'nome' && jogoStatus.jogador1 !== '') {
+      jogoStatus.jogador2 = msg.dado
+      console.log('jogador 2', jogoStatus.jogador2)
+    }
+
+    if(msg.tipo === 'novo_jogo'){
+      jogoStatus = {
+        jogador1: "",
+        jogador2: "",
+        jogadas: 0
+     }
+    }
+
+
 
     // verificar tipo de mensagem (nome|joga)
     // se tipo for nome
@@ -21,8 +48,6 @@ io.on('connection', function(socket){
     // se tipo for jogada
     // atualiza quantidade de jogada
 
-
-    console.log('jogo_acao: ' + msg);
   });
 })
 
--- public/js/jogodavelha.js
+++ public/js/jogodavelha.js
@@ -1,17 +1,34 @@
-let jogador1
-let jogador2
+let jogador
 
+//let iniciado = false 
 
 const socket = io('http://localhost:3000')
-socket.on('connect', function () {
-    jogador1 = prompt(`Qual é o seu nome jogador 1?`)
-    jogador2 = prompt(`QUal é o seu nome jogador 2?`)
-})
+
+socket.on('connect', function () { })
 socket.on('event', function (data) { })
 socket.on('disconnect', function () { })
 
+const comecar = document.querySelector('.btn-iniciar').addEventListener('click', iniciar)
+
+socket.on('socket_msg', function (msg) {
+    if (msg.tipo === 'jogador1') {
+        comecar.innerText.style.display = 'none'
+    }
+
+    if (msg.tipo === 'jogador1' && jogador !== msg.dado) {
+        jogador = prompt(`Qual é o seu nome jogador 2?`)
+        socket.emit('jogo_acao', { tipo: 'nome', dado: jogador })
+    }
+});
+
 function sair() {
     alert(`Tem certeza? Todo o seu progresso irá ser perdido`)
+}
+function iniciar() {
+    //socket.emit('jogo_acao', { tipo: 'novo_jogo' })
+
+    jogador = prompt(`Qual é o seu nome jogador 1?`)
+    socket.emit('jogo_acao', { tipo: 'nome', dado: jogador })
 }
 
 let jogada = 0;
@@ -25,78 +42,72 @@ let ganha = document.querySelector('.ganhador')
 let imgGanha = document.querySelector('.trofeu')
 let imgperde = document.querySelector('.velhaft')
 
-
 function verificar() {
 
     const foto = event.target.querySelector('img');
     const div = foto.closest('.box')
 
-    if (jogador1 == null && jogador2 == null) {
-        jogador1 = prompt(`Favor informar o nome do jogador 1?`)
-        jogador2 = prompt(`Favor informar o nome do jogador 2?`)
+    if (jogada > 8) {
+        alert(`O jogo acabou!`)
+    }
+    if (div.classList.contains("clicado")) {
+        alert(`PROIBIDO ROUBAR`)
     } else {
-
-
-
-        if (jogada > 8) {
-            alert(`O jogo acabou!`)
-        }
-        if (div.classList.contains("clicado")) {
-            alert(`PROIBIDO ROUBAR`)
-        } else {
-            jogada++;
-            socket.emit('jogo_acao', `${jogador1} jogou`)
-            console.log(`${jogador1} fez uma movimentação`)
-            const imgName = (jogada % 2) ? 'ex' : 'ball';
-            foto.setAttribute('src', '../imagens/' + imgName + '.png');
-            div.classList.add("clicado")
-
-            if (imgName == 'ex') {
-                div.classList.add("xis")
-            }
-            else {
-                div.classList.add("bolinha")
-            }
-
-        }
-
-        if (
-            box[0].classList.contains('xis') && box[1].classList.contains('xis') && box[2].classList.contains('xis')
-            || box[3].classList.contains('xis') && box[4].classList.contains('xis') && box[5].classList.contains('xis')
-            || box[6].classList.contains('xis') && box[7].classList.contains('xis') && box[8].classList.contains('xis')
-            || box[0].classList.contains('xis') && box[3].classList.contains('xis') && box[6].classList.contains('xis')
-            || box[1].classList.contains('xis') && box[4].classList.contains('xis') && box[7].classList.contains('xis')
-            || box[2].classList.contains('xis') && box[5].classList.contains('xis') && box[8].classList.contains('xis')
-            || box[0].classList.contains('xis') && box[4].classList.contains('xis') && box[8].classList.contains('xis')
-            || box[2].classList.contains('xis') && box[4].classList.contains('xis') && box[6].classList.contains('xis')
-        ) {
-            venc.classList.add('X')
-            ganhador()
-
-        } else if (box[0].classList.contains('bolinha') && box[1].classList.contains('bolinha') && box[2].classList.contains('bolinha')
-            || box[3].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[5].classList.contains('bolinha')
-            || box[6].classList.contains('bolinha') && box[7].classList.contains('bolinha') && box[8].classList.contains('bolinha')
-            || box[0].classList.contains('bolinha') && box[3].classList.contains('bolinha') && box[6].classList.contains('bolinha')
-            || box[1].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[7].classList.contains('bolinha')
-            || box[2].classList.contains('bolinha') && box[5].classList.contains('bolinha') && box[8].classList.contains('bolinha')
-            || box[0].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[8].classList.contains('bolinha')
-            || box[2].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[6].classList.contains('bolinha')) {
-            venc.classList.add('O')
-            ganhador()
+        jogada++;
+        const imgName = (jogada % 2) ? 'ex' : 'ball';
+        foto.setAttribute('src', '../imagens/' + imgName + '.png');
+        div.classList.add("clicado")
+        // debugger
+        if (imgName == 'ex') {
+            div.classList.add("xis")
+            // socket.emit('jogo_acao', `${jogador1} jogou`)
+            // NÃO ESTAVA DEIXANDO O POP-UP APARECER 
         }
         else {
-            if (box[0].classList.contains('clicado')
-                && box[1].classList.contains('clicado')
-                && box[2].classList.contains('clicado')
-                && box[3].classList.contains('clicado')
-                && box[4].classList.contains('clicado')
-                && box[5].classList.contains('clicado')
-                && box[6].classList.contains('clicado')
-                && box[7].classList.contains('clicado')
-                && box[8].classList.contains('clicado')
-            ) {
-                perdeu()
-            }
+            div.classList.add("bolinha")
+            // socket.emit('jogo_acao', `${jogador2} jogou`)
+            // NÃO ESTAVA DEIXANDO O POP-UP APARECER 
+
+        }
+
+    }
+
+    if (
+        box[0].classList.contains('xis') && box[1].classList.contains('xis') && box[2].classList.contains('xis')
+        || box[3].classList.contains('xis') && box[4].classList.contains('xis') && box[5].classList.contains('xis')
+        || box[6].classList.contains('xis') && box[7].classList.contains('xis') && box[8].classList.contains('xis')
+        || box[0].classList.contains('xis') && box[3].classList.contains('xis') && box[6].classList.contains('xis')
+        || box[1].classList.contains('xis') && box[4].classList.contains('xis') && box[7].classList.contains('xis')
+        || box[2].classList.contains('xis') && box[5].classList.contains('xis') && box[8].classList.contains('xis')
+        || box[0].classList.contains('xis') && box[4].classList.contains('xis') && box[8].classList.contains('xis')
+        || box[2].classList.contains('xis') && box[4].classList.contains('xis') && box[6].classList.contains('xis')
+    ) {
+        venc.classList.add('X')
+        ganhador()
+
+    } else if (box[0].classList.contains('bolinha') && box[1].classList.contains('bolinha') && box[2].classList.contains('bolinha')
+        || box[3].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[5].classList.contains('bolinha')
+        || box[6].classList.contains('bolinha') && box[7].classList.contains('bolinha') && box[8].classList.contains('bolinha')
+        || box[0].classList.contains('bolinha') && box[3].classList.contains('bolinha') && box[6].classList.contains('bolinha')
+        || box[1].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[7].classList.contains('bolinha')
+        || box[2].classList.contains('bolinha') && box[5].classList.contains('bolinha') && box[8].classList.contains('bolinha')
+        || box[0].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[8].classList.contains('bolinha')
+        || box[2].classList.contains('bolinha') && box[4].classList.contains('bolinha') && box[6].classList.contains('bolinha')) {
+        venc.classList.add('O')
+        ganhador()
+    }
+    else {
+        if (box[0].classList.contains('clicado')
+            && box[1].classList.contains('clicado')
+            && box[2].classList.contains('clicado')
+            && box[3].classList.contains('clicado')
+            && box[4].classList.contains('clicado')
+            && box[5].classList.contains('clicado')
+            && box[6].classList.contains('clicado')
+            && box[7].classList.contains('clicado')
+            && box[8].classList.contains('clicado')
+        ) {
+            perdeu()
         }
     }
 }
