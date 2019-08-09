af265cc64b9e6753f993ba9b2fe20e1b83c843ef
Guilherme Nunes Mingardi <guilherme.mingardi@easynvest.com.br>
2019-08-09 15:44:15 -0300

    Corrige player jogar duas vezes seguidas, pop-up esta aparecendo corretamente e zera as jogadas apos selecionar recomecar

--- public/js/jogodavelha.js
+++ public/js/jogodavelha.js
@@ -1,5 +1,5 @@
 let jogador
-
+let eh_jogador1 = true
 let jogada = 0
 const box = document.querySelectorAll('.box')
 const pop = document.querySelector('.pop_up')
@@ -11,8 +11,29 @@ let imgGanha = document.querySelector('.trofeu')
 let imgperde = document.querySelector('.velhaft')
 let player = document.querySelector('.player')
 const matriz = document.querySelector('.matriz')
+const restart = document.querySelector('.restart')
+restart.addEventListener('click', function () {
+    // fechar caixa de pop-up 
+    pop.style.display = 'none'
+    bloco.style.display = 'block'
+    matriz.style.display ='block'
+    // remover as jogadas 
 
-//let iniciado = false 
+    console.log('limpa')
+    for (let i = 0; i < box.length; i++) {
+        box[i].classList.remove('clicado')
+        box[i].classList.remove('bolinha')
+        box[i].classList.remove('xis')
+        box[i].classList.remove('O')
+        box[i].classList.remove('X')
+        jogada = 0
+        const foto = box[i].querySelector('img')
+        foto.setAttribute('src', '')
+    }
+})
+
+
+// let block 
 
 const socket = io('http://localhost:3000')
 
@@ -22,41 +43,6 @@ socket.on('disconnect', function () { })
 
 const comecar = document.querySelector('.btn-iniciar')
 comecar.addEventListener('click', iniciar)
-
-socket.on('socket_msg', function (msg) {
-    if (msg.tipo === 'jogador1') {
-        //console.log('player 1', msg.dado)
-    }
-
-    // logica do jogador 2
-    if (msg.tipo === 'jogador1' && jogador !== msg.dado) {
-        jogador = prompt(`Qual é o seu nome jogador 2?`)
-        socket.emit('jogo_acao', { tipo: 'nome', dado: jogador })
-        comecar.style.display = 'none'
-        matriz.style.display = 'block'
-        player.innerText = jogador
-    }
-
-    // logica das jogadas
-    if (msg.tipo === 'jogada') {
-        jogada = msg.dado.num_jogada
-
-        const div = document.querySelector(`div[data-index="${msg.dado.index}"]`)
-        const foto = div.querySelector('img')
-        const imgName = (msg.dado.num_jogada % 2) ? 'ex' : 'ball';
-
-        foto.setAttribute('src', '../imagens/' + imgName + '.png');
-        div.classList.add("clicado")
-
-        if (imgName == 'ex') {
-            div.classList.add("xis")
-        }
-        else {
-            div.classList.add("bolinha")
-        }
-    }
-
-});
 
 function sair() {
     alert(`Tem certeza? Todo o seu progresso irá ser perdido`)
@@ -71,32 +57,45 @@ function iniciar() {
     comecar.style.display = 'none'
     matriz.style.display = 'block'
     player.innerText = jogador
-
+    // block = false 
+    console.log(block)
 }
 
+socket.on('socket_msg', function (msg) {
+    // logica do jogador 2
+    if (msg.tipo === 'jogador1' && jogador !== msg.dado) {
+        eh_jogador1 = false
 
-
-function verificar(e) {
-    const foto = event.target.querySelector('img');
-    const div = foto.closest('.box')
-
-    if (jogada > 8) {
-        alert(`O jogo acabou!`)
-    }
-    if (div.classList.contains("clicado")) {
-        alert(`PROIBIDO ROUBAR`)
-    } else {
-        jogada++;
+        jogador = prompt(`Qual é o seu nome jogador 2?`)
+        socket.emit('jogo_acao', { tipo: 'nome', dado: jogador })
+        comecar.style.display = 'none'
+        matriz.style.display = 'block'
+        player.innerText = jogador
     }
 
-    socket.emit('jogo_acao', {
-        tipo: 'jogada',
-        dado: {
-            num_jogada: jogada,
-            index: e.target.getAttribute('data-index')
+    // logica das jogadas
+    if (msg.tipo === 'jogada') {
+        jogada = msg.dado.num_jogada
+        const div = document.querySelector(`div[data-index="${msg.dado.index}"]`)
+        const foto = div.querySelector('img')
+        const imgName = (msg.dado.num_jogada % 2) ? 'ex' : 'ball'
+
+        foto.setAttribute('src', '../imagens/' + imgName + '.png')
+        div.classList.add("clicado")
+
+        if (imgName == 'ex') {
+            div.classList.add("xis")
         }
-    })
+        else {
+            div.classList.add("bolinha")
+        }
+        verificacao()
 
+    }
+
+});
+
+function verificacao() {
     if (
         box[0].classList.contains('xis') && box[1].classList.contains('xis') && box[2].classList.contains('xis')
         || box[3].classList.contains('xis') && box[4].classList.contains('xis') && box[5].classList.contains('xis')
@@ -137,7 +136,35 @@ function verificar(e) {
     }
 }
 
+function verificar(e) {
+    const div = event.target
+
+    if (eh_jogador1 && jogada % 2 === 1) {
+        return
+    }
+
+    if (!eh_jogador1 && jogada % 2 === 0) {
+        return
+    }
+    if (div.classList.contains("clicado")) {
+        alert(`PROIBIDO ROUBAR`)
+    } else {
+        jogada++;
+
+        socket.emit('jogo_acao', {
+            tipo: 'jogada',
+            dado: {
+                num_jogada: jogada,
+                index: e.target.getAttribute('data-index')
+            }
+        })
+        block = true
+    }
+}
+
 function ganhador() {
+    // socket.emit('jogo_acao', { tipo: 'zerar_jogo', dado:  })
+
     pop.style.display = 'block'
     bloco.style.display = 'none'
     imgperde.style.display = 'none'
